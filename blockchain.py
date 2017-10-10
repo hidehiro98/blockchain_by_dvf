8dcfb57f82037b1c8e7a8d486ad6920b67c6cc68
Hidehiro Nagaoka <kuinige@gmail.com>
2017-10-10 19:22:27 +0900

    Flaskのためのボイラープレートコードを追加

--- blockchain.py
+++ blockchain.py
@@ -2,9 +2,11 @@
 
 import hashlib
 import json
-
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+from flask import flask
 
 class Blockchain(object):
     def __init__(self):
@@ -97,3 +99,37 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest
 
         return guess_hash[:4] == "0000"
+
+
+# ノードを作る
+# Flaskについて詳しくはこちらを読んでほしい http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
+app = Flask(__name__)
+
+# このノードのグローバルにユニークなアドレスを作る
+node_identifire = str(uuid4()).replace('-', '')
+
+# ブロックチェーンクラスをインスタンス化する
+blockchain = Blockchain()
+
+# メソッドはPOSTで/transactions/newエンドポイントを作る。メソッドはPOSTなのでデータを送信する
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    return '新しいトランザクションを追加します'
+
+# メソッドはGETで/mineエンドポイントを作る
+@app.route('/mine', methods=['GET'])
+def mine():
+    return '新しいブロックを採掘します'
+
+# メソッドはGETで、フルのブロックチェーンをリターンする/chainエンドポイントを作る
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+# port5000でサーバーを起動する
+if __name__ == __main__:
+    app.run(host='0.0.0.0', port=5000)
